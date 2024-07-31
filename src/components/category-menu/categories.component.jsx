2e8226063876723a6914f668fe4bdf77d5e972c4
stefanm95 <stefan.moise95@icloud.com>
2024-07-31 14:46:40 +0300

    updating categories page

---
+++ src/components/category-menu/categories.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CategoryItem from '../category-item/category-item.component'
+import './categories.styles.scss'
+const Categories = ({categories}) => {
+  return (
+    <div className="categories-container">
+    {categories.map(({ title, id, imageUrl }) => (
+      <CategoryItem key={id} category={{ title, id, imageUrl }} />
+    ))}
+  </div>
+  )
+}
+
+export default Categories
